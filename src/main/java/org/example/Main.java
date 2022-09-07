a60a7aa95c5e276dfe8c8408fa7562a07b3a171e
hannahchun <hannahchun2000@gmail.com>
2022-09-07 12:05:35 +0900

    fourth commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Nice to meet you!");
         System.out.println("How have you been lately?");
+
+        int first = 10;
     }
 }
