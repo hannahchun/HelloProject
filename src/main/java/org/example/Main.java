fc4ca2d79e079a807f8ef39b70a7afcf21930ad2
hannahchun <hannahchun2000@gmail.com>
2022-09-07 11:51:06 +0900

    third commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Nice to meet you!");
+        System.out.println("How have you been lately?");
     }
 }
